4c9042e0cbda7f49a8125de83d2c7d58f824d512
niksa <niksahn@gmail.com>
2024-05-27 20:17:13 +0300

    Добавьте файлы проекта.

---
+++ VA 7/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace VA_7 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(603, 432);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(503, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(217, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(502, 152);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(217, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(502, 217);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(217, 29);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(502, 286);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(217, 29);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(502, 361);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(217, 29);
+            this.textBox5.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 8;
+            this.numericUpDown2.Location = new System.Drawing.Point(116, 175);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            524288});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(107, 29);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            524288});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 175);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Точность: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "∫(1+sin2x)e^(-x)  [1 ; 2]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Решение:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 21);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Прямоугольников (левых):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(251, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Прямоугольников (средних):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(250, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Прямоугольников (правых):";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(250, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 21);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Трапеций:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(251, 361);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 21);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Симпсона:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(842, 28);
+            this.label9.Name = "label9";
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label9.Size = new System.Drawing.Size(109, 21);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Погрешность:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(821, 80);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(246, 29);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(820, 149);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(246, 29);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(820, 214);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(246, 29);
+            this.textBox8.TabIndex = 19;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(820, 283);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(246, 29);
+            this.textBox9.TabIndex = 20;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(820, 358);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(246, 29);
+            this.textBox10.TabIndex = 21;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1102, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(208, 61);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(1102, 135);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(208, 61);
+            this.richTextBox2.TabIndex = 23;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(1102, 202);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(208, 61);
+            this.richTextBox3.TabIndex = 24;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(1102, 269);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(208, 61);
+            this.richTextBox4.TabIndex = 25;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(1102, 336);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(208, 61);
+            this.richTextBox5.TabIndex = 26;
+            this.richTextBox5.Text = "";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1141, 28);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 21);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Таблица итераций:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1345, 508);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сахно лаба 7";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ VA 7/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VA_7 {
+    public delegate void fun(double a);
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public delegate double IntegrationMethod(double a, double b, int n);
+
+        public static class RungeRule {
+            public static double ErrorMediumRect(double a, double b, int n) {
+                double I_n = MediumRectMethod(a, b, n);
+                double I_2n = MediumRectMethod(a, b, 2 * n);
+                return Math.Abs(I_2n - I_n) / 3;
+            }
+
+            public static double ErrorLeftRect(double a, double b, int n) {
+                double I_n = LeftRectMethod(a, b, n);
+                double I_2n = LeftRectMethod(a, b, 2 * n);
+                return Math.Abs(I_2n - I_n);
+            }
+
+            public static double ErrorRightRect(double a, double b, int n) {
+                double I_n = RightRectMethod(a, b, n);
+                double I_2n = RightRectMethod(a, b, 2 * n);
+                return Math.Abs(I_2n - I_n);
+            }
+
+            public static double ErrorTrapezoid(double a, double b, int n) {
+                double I_n = TrapezoidMethod(a, b, n);
+                double I_2n = TrapezoidMethod(a, b, 2 * n);
+                return Math.Abs(I_2n - I_n) / 3;
+            }
+
+            public static double ErrorSimpsons(double a, double b, int n) {
+                double I_n = SimpsonsMethod(a, b, n);
+                double I_2n = SimpsonsMethod(a, b, 2 * n);
+                return Math.Abs(I_2n - I_n) / 15;
+            }
+        }
+
+        public static double Integrand(double x) {
+            return (1+Math.Sin(2*x))*Math.Pow(Math.E,-x);
+        }
+
+        public static double MediumRectMethod(double a, double b, int n) {
+            double h = (b - a) / n;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                double xi = a + (i + 0.5) * h;
+                sum += Integrand(xi);
+            }
+
+            return h * sum;
+        }
+
+        public static double LeftRectMethod(double a, double b, int n) {
+            double h = (b - a) / n;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                double xi = a + i * h;
+                sum += Integrand(xi);
+            }
+
+            return h * sum;
+        }
+
+        public static double RightRectMethod(double a, double b, int n) {
+            double h = (b - a) / n;
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                double xi = a + i * h;
+                sum += Integrand(xi);
+            }
+
+            return h * sum;
+        }
+
+        public static double TrapezoidMethod(double a, double b, int n) {
+            double h = (b - a) / n;
+            double sum = 0.5 * (Integrand(a) + Integrand(b));
+
+            for (int i = 1; i < n; i++) {
+                double xi = a + i * h;
+                sum += Integrand(xi);
+            }
+
+            return h * sum;
+        }
+
+        public static double SimpsonsMethod(double a, double b, int n) {
+            if (n % 2 != 0) {
+                throw new ArgumentException("Для метода Симпсона кол-во интервалов должно быть четным числом");
+            }
+
+            double h = (b - a) / n;
+            double sum = Integrand(a) + Integrand(b);
+
+            for (int i = 1; i < n; i += 2) {
+                double xi = a + i * h;
+                sum += 4 * Integrand(xi);
+            }
+
+            for (int i = 2; i < n - 1; i += 2) {
+                double xi = a + i * h;
+                sum += 2 * Integrand(xi);
+            }
+
+            return h * sum / 3;
+        }
+
+        private async void button1_Click(object sender, EventArgs e) {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            richTextBox4.Text = "";
+            richTextBox5.Text = "";
+            double a = 1;
+            double b =2;
+            int n = 2;
+            double accuracy = (double)numericUpDown2.Value;
+
+            double midpointResult, midpointError,
+                   leftRectangleResult, leftRectangleError,
+                   rightRectangleResult, rightRectangleError,
+                   trapezoidalResult, trapezoidalError,
+                   simpsonsResult, simpsonsError;
+
+            var midpointTask = Task.Run(() => CalculateWithAccuracy(MediumRectMethod, a, b, n, accuracy, RungeRule.ErrorMediumRect, 
+           (i) =>
+           {
+                richTextBox1.Invoke(new Action(() => { richTextBox1.AppendText(i + "\n"); }));
+           }
+           )
+            );
+            var leftRectangleTask = Task.Run(() => CalculateWithAccuracy(LeftRectMethod, a, b, n, accuracy, RungeRule.ErrorLeftRect, (i) =>
+            {
+                richTextBox2.Invoke(new Action(() => { richTextBox2.AppendText( i + "\n"); }));
+            }));
+            var rightRectangleTask = Task.Run(() => CalculateWithAccuracy(RightRectMethod, a, b, n, accuracy, RungeRule.ErrorRightRect, (i) =>
+            {
+                richTextBox3.Invoke(new Action(() => { richTextBox3.AppendText(i + "\n"); }));
+            }));
+            var trapezoidalTask = Task.Run(() => CalculateWithAccuracy(TrapezoidMethod, a, b, n, accuracy, RungeRule.ErrorTrapezoid, (i) => { richTextBox4.Invoke(new Action(() => { richTextBox4.AppendText(i + "\n"); })); }));
+            try {
+                var simpsonsTask = Task.Run(() => CalculateWithAccuracy(SimpsonsMethod, a, b, n, accuracy, RungeRule.ErrorSimpsons, (i) => { richTextBox5.Invoke(new Action(() => { richTextBox5.AppendText(i+"\n"); })); }));
+                await Task.WhenAll(midpointTask, leftRectangleTask, rightRectangleTask, trapezoidalTask, simpsonsTask);
+
+                midpointResult = midpointTask.Result.Item1;
+                midpointError = midpointTask.Result.Item2;
+
+                leftRectangleResult = leftRectangleTask.Result.Item1;
+                leftRectangleError = leftRectangleTask.Result.Item2;
+
+                rightRectangleResult = rightRectangleTask.Result.Item1;
+                rightRectangleError = rightRectangleTask.Result.Item2;
+
+                trapezoidalResult = trapezoidalTask.Result.Item1;
+                trapezoidalError = trapezoidalTask.Result.Item2;
+
+                simpsonsResult = simpsonsTask.Result.Item1;
+                simpsonsError = simpsonsTask.Result.Item2;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            textBox1.Text = midpointResult.ToString();
+            textBox2.Text = leftRectangleResult.ToString();
+            textBox3.Text = rightRectangleResult.ToString();
+            textBox4.Text = trapezoidalResult.ToString();
+            textBox5.Text = simpsonsResult.ToString();
+
+            textBox6.Text = midpointError.ToString();
+            textBox7.Text = leftRectangleError.ToString();
+            textBox8.Text = rightRectangleError.ToString();
+            textBox9.Text = trapezoidalError.ToString();
+            textBox10.Text = simpsonsError.ToString();
+        }
+
+        private (double result, double error) CalculateWithAccuracy(Func<double, double, int, double> integrationMethod, double a, double b, int n, double accuracy, Func<double, double, int, double> errorMethod, fun fillTable) {
+            double result;
+            double error;
+            do {
+                error = errorMethod(a, b, n);
+                n *= 2;
+                result = integrationMethod(a, b, n);
+                fillTable(result);
+            } while (error > accuracy);
+
+            return (result, error);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
